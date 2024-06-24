2a0bdaa57a030fc091b692e0311c6dc3ff06a6c0
RicardoR <75104611+rik133@users.noreply.github.com>
2024-06-24 19:58:41 -0300

    Add files via upload

--- pygm.py
+++ pygm.py
@@ -6,11 +6,11 @@ import random
 import os
 import math
 from scripts.utility import get_image, get_images, get_imag_dir, Animation
-from scripts.ent import PhysicsEntity, Player
+from scripts.ent import PhysicsEntity, Player, Enemy
 from scripts.tilemap import Tilemap
 from scripts.clouds import Clouds
 from scripts.particle import Particle
-
+from scripts.faisca import Faisca
 
 class game():
     def __init__(self):
@@ -34,6 +34,8 @@ class game():
             'player/walk' : Animation(get_images('player/walk-sheet.png', spritesheetsize = [32*4, 32], spritesize = [32, 32], spritsheetstart= [0, 0]), ani_dur = 5),
             'particle/leaf': Animation(get_imag_dir('particles/leaf'), ani_dur=40, loop=False),
             'portal': get_images('mundo/portal.png', spritesheetsize = [32, 32], spritesize = [32, 32], spritsheetstart= [0, 0]),
+            'gun': get_image('gun.png'),
+            'projectile': get_image('projectile.png'),
         }
         
         
@@ -52,7 +54,7 @@ class game():
         self.level = 0
         
         self.load_level(self.level)
-        self.cameramove = [0,0]
+        
         
     def load_level(self, level):
         self.tilemap.load('pygame-listaencadeada/dados/mapas/' + str(level) + '.json')
@@ -64,9 +66,22 @@ class game():
             self.leaf_spawners.append(pygame.Rect(4 + tree['pos'][0], 4 + tree['pos'][1], 32, 20))
         for saida in self.tilemap.extract([('portal', 0)], keep=True):
             self.saidas.append(pygame.Rect(saida['pos'][0], saida['pos'][1], 16, 16))
+        
+        
+        self.enemies = []
+        for spawner in self.tilemap.extract([('spawners', 0), ('spawners', 1)]):
+            if spawner['variant'] == 0:
+                self.player.pos = spawner['pos']
+                self.player.air_time = 0
+            else:
+                self.enemies.append(Enemy(self, spawner['pos'], (8, 15)))
+            
+        self.projectiles = []
         self.particles = []
+        self.sparks = []
         
-        
+        self.cameramove = [0,0]
+        self.dead = 0
         self.transition = -30
         self.player.terminou = False
         
@@ -100,9 +115,42 @@ class game():
             self.clouds.update()
             self.clouds.render(self.camera, offset=render_camove)
             self.tilemap.render(self.camera, offset=render_camove)
-            
-            self.player.update(self.tilemap, (self.mov[1] - self.mov[0], 0))
-            self.player.render(self.camera, offset=render_camove)
+            if not self.dead:
+                self.player.update(self.tilemap, (self.mov[1] - self.mov[0], 0))
+                self.player.render(self.camera, offset=render_camove)
+            for projectile in self.projectiles.copy():
+                projectile[0][0] += projectile[1]
+                projectile[2] += 1
+                img = self.assets['projectile']
+                self.display.blit(img, (projectile[0][0] - img.get_width() / 2 - render_camove[0], projectile[0][1] - img.get_height() / 2 - render_camove[1]))
+                if self.tilemap.solid_check(projectile[0]):
+                    self.projectiles.remove(projectile)
+                    for i in range(4):
+                        self.sparks.append(faisca(projectile[0], random.random() - 0.5 + (math.pi if projectile[1] > 0 else 0), 2 + random.random()))
+                elif projectile[2] > 360:
+                    self.projectiles.remove(projectile)
+                elif abs(self.player.dashing) < 50:
+                    if self.player.rect().collidepoint(projectile[0]):
+                        self.projectiles.remove(projectile)
+                        self.dead += 1
+                        self.screenshake = max(16, self.screenshake)
+                        for i in range(30):
+                            angle = random.random() * math.pi * 2
+                            speed = random.random() * 5
+                            self.sparks.append(faisca(self.player.rect().center, angle, 2 + random.random()))
+                            self.particles.append(Particle(self, 'particle', self.player.rect().center, velocity=[math.cos(angle + math.pi) * speed * 0.5, math.sin(angle + math.pi) * speed * 0.5], frame=random.randint(0, 7)))
+   
+
+
+
+
+
+
+            for faisca in self.sparks.copy():
+                kill = faisca.update()
+                faisca.render(self.camera, offset=render_camove)
+                if kill:
+                    self.sparks.remove(faisca)
             
             for particle in self.particles.copy():
                 kill = particle.update()
@@ -146,69 +194,6 @@ class game():
             pygame.display.update()
             self.fps.tick(60)
             
-'''class tela():
-    def __init__(self):
-        self.width = 640
-        self.height = 480
-        
-        self.image = pygame.draw.rect(self.screen, (5, 144, 252), (0, 0, 640, 480))
-
-        
-   
-    def update(self):
-        self.screen.fill((5, 144, 252))
-        
-class player():
-    
-    def __init__(self):
-        self.img_pos = [320, 240]
-        self.x = self.img_pos[0]
-        self.y = self.img_pos[1]
-        self.speed = 0.9
-        self.xlr8 = 5
-        self.life = 3
-        self.image = pygame.image.load('imagens pygame\player\idel.png')
-        
-        self.img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
-        
-        
-    def move(self, event):
-        
-        
-        
-        
-        
-        keys = pygame.key.get_pressed()
-        if self.xlr8 > 10:
-            self.xlr8 -= 2
-        if keys[K_LEFT] and self.x > 5:
-            self.x -= self.speed*self.xlr8
-            
-            self.xlr8 += 1
-            print('esquerda')
-        if keys[K_RIGHT] and self.x < 612:
-            self.x += self.speed*self.xlr8
-            
-            self.xlr8 += 1
-            print('direita')
-        if keys[K_UP] and self.y > 5:
-            self.y -= self.speed*self.xlr8
-            
-            self.xlr8 += 1
-            print('cima')
-        if keys[K_DOWN] and self.y < 453:
-            self.y += self.speed*self.xlr8
-            
-            self.xlr8 += 1
-            print('baixo')
-        
-    def update(self):
-        self.img_pos[0] += (self.mov[1] - self.mov[0]) * 10
-        self.img_pos[1] += (self.mov[3] - self.mov[2]) * 10
-        
-    
-        
-'''
             
 game().run()
 
